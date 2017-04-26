18882dba5d94ce8ffb921e798cf6580cdaba1562
manish <manijain333@gmail.com>
2017-04-27 02:41:29 +0530

    minor changes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,10 +3,23 @@ class ProductsController < InheritedResources::Base
     @products = Product.all.paginate(:page => params[:page])
   end
 
+  def create
+  	debugger
+  	@product = Product.new(product_params)
+  	respond_to do |format|
+      if @product.save
+        format.html { redirect_to product_path(@product), notice: 'Product was successfully created.' }
+        format.json { render :show, status: :created, location: @product }
+      else
+        format.html { render :new }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
     def product_params
       params.require(:product).permit(:title, :description, :price, :quantity, :picture)
     end
 end
-
