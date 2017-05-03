763a6a1a79a9ff5a5c21a98b57a6e669ace215fd
Manish <manish.jain2@globalogic.com>
2017-05-03 18:03:26 +0530

    added admin user in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Customer.create(name: 'admin', email: "admin@example.com", password: "password", password_confirmation: "password", is_admin: "1")
